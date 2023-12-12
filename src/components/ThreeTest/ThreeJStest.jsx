7ec15c261851e6b87579e94ab7e9e5ba67ad1853
Ian Sebelius <sebeliusancira@gmail.com>
2023-12-11 22:49:34 -0800

    change cube rotation

--- src/components/ThreeTest/ThreeJStest.jsx
+++ src/components/ThreeTest/ThreeJStest.jsx
@@ -22,8 +22,8 @@ function ThreeJSTest() {
     const animate = () => {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.02;
-      cube.rotation.y += 0.01;
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.02;
 
       renderer.render(scene, camera);
     };

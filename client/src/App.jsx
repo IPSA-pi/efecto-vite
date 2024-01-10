4be6f9caf8bd6d342e56c33cf817de6d456f46b8
Ian Sebelius <sebeliusancira@gmail.com>
2024-01-09 19:20:29 -0600

    create login component and add it to app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Home from './components/Home'
 import JergaCreate from './components/JergaCreate/JergaCreate'
 import JergaRelok from './components/JergaRelok/JergaRelok'
 // import About from './components/About'
+import Login from './components/Login/Login'
 import './App.css'
 import './styles/global.css'
 import './styles/themes.css'
@@ -23,6 +24,7 @@ function App() {
         <Link to="/">Raíz</Link>
         <Link to="/jergaCreate">JergaCreate</Link>
         <Link to="/jergaRelok">JergaRelok</Link>
+        <Login />
         {/* <Link to="/about">AcercaDe</Link> */}
       </nav>
       <Routes>
@@ -36,8 +38,7 @@ function App() {
         <button onClick={toggleTheme}>Toggle Theme</button>
       </footer>
     </Router>
-    
-  )
+  );
 }
 
 export default App

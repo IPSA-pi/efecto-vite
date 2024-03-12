91f5d929ba2b391ca508d4005392a862ffa12325
Ian Sebelius <sebeliusancira@gmail.com>
2024-03-11 22:07:24 -0700

    add date format to jergaRelok, refactor styles

--- src/utils/binaryTime.js
+++ src/utils/binaryTime.js
@@ -1,5 +1,8 @@
 const binaryTime = (time, timeFormat) => {
-  const [hour, minutes, seconds, milliseconds] = [
+  const [year, month, day, hour, minutes, seconds, milliseconds] = [
+    time.getFullYear(),
+    time.getMonth() + 1,
+    time.getDate(),
     time.getHours(), 
     time.getMinutes(), 
     time.getSeconds(), 
@@ -10,25 +13,32 @@ const binaryTime = (time, timeFormat) => {
   let formatTime = (timeFormat === 'ms')
     ? time.getTime().toString()
     : [
+        year.toString().padStart(2, '0'),
+        month.toString().padStart(2, '0'),
+        day.toString().padStart(2, '0'),
         hour.toString().padStart(2, '0'), 
         minutes.toString().padStart(2, '0'), 
         seconds.toString().padStart(2, '0')
       ].join('') + (timeFormat === 'hmsm' ? milliseconds.toString().padStart(3, '0') : '');
-    
   switch (timeFormat) {
-    case 'ms':
-    case 'hmsm':
+    case 'full':
       return formatTime;
+    case 'ms':
+      return formatTime;
+    case 'hmsm':
+      return formatTime.slice(8);
 
     case 'hms':  
-      return formatTime;
+      return formatTime.slice(8);
 
     case 'hm':  
-      return formatTime.slice(0,-2);
+      return formatTime.slice(8, -2);
 
     default:
       throw new Error('Invalid time format');
   }
 }; 
+const dateTest = new Date();
+binaryTime(dateTest, 'hmsm');
 
 export { binaryTime };
